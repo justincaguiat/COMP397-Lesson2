70c34db81c6f0b24d2d386d1a5f4d51863c6a461
justincaguiat <justin.caguiat@gmail.com>
2015-01-12 13:39:35 -0500

    first commit ever!

---
+++ Comp397-lesson02/Scripts/game.js
@@ -0,0 +1,12 @@
+﻿var Player = (function () {
+    function Player() {
+        this.strength = 10;
+    }
+    Player.prototype.fights = function () {
+        console.log("He Fights");
+    };
+    return Player;
+})();
+
+var tom = new Player();
+tom.fights();
---
+++ Comp397-lesson02/Scripts/game.ts
@@ -0,0 +1,16 @@
+﻿class Player {
+    strength: number;
+    constructor() {
+        this.strength = 10;
+    }
+
+    fights() {
+        console.log("He Fights");
+    }
+
+} 
+
+function main() {
+    var tom = new Player();
+    tom.fights();
+}
